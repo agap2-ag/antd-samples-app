3216d5e5bc563ac6aacbaa5295dbbf025166f165
Alexandre Gouveia <agouveia@agap2.pt>
2019-03-29 17:26:44 +0000

    Updates

---
+++ precache-manifest.63b7b5db0ab82cae29a47ad405ba9442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/antd-samples-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e23deba0fe92d186d5a",
+    "url": "/antd-samples-app/static/js/runtime~main.bf6aab00.js"
+  },
+  {
+    "revision": "cb7889a1018625db9d0c",
+    "url": "/antd-samples-app/static/js/main.65a50e1b.chunk.js"
+  },
+  {
+    "revision": "3e7da128c8e58e6070e4",
+    "url": "/antd-samples-app/static/js/2.0feeaabd.chunk.js"
+  },
+  {
+    "revision": "cb7889a1018625db9d0c",
+    "url": "/antd-samples-app/static/css/main.82e0c1d5.chunk.css"
+  },
+  {
+    "revision": "3e7da128c8e58e6070e4",
+    "url": "/antd-samples-app/static/css/2.329174fe.chunk.css"
+  },
+  {
+    "revision": "a05c343492807fc7ab6c4bb40dfc5f39",
+    "url": "/antd-samples-app/index.html"
+  }
+];
